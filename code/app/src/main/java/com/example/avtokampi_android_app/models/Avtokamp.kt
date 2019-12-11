73442b5da6bec7b8d10ab8d026e6a21599e19d7f
Anže Luzar <anze.luzar@xlab.si>
2019-12-11 09:59:02 +0100

    Refactor data models.

--- code/app/src/main/java/com/example/avtokampi_android_app/models/Avtokamp.kt
+++ code/app/src/main/java/com/example/avtokampi_android_app/models/Avtokamp.kt
@@ -1,10 +1,17 @@
 package com.example.avtokampi_android_app.models
 
+import com.google.android.gms.maps.model.LatLng
+
 data class Avtokamp(
     val id: Int,
     val naziv: String,
     val opis: String,
     val naslov: String,
     val telefon: String,
-    val lokacija: Lokacija
+    val naziv_lokacije: String,
+    val isActive: Boolean,
+    val lokacija: LatLng,
+    val createdAt: String,
+    val updatedAt: String,
+    val regija: Regija
 )
